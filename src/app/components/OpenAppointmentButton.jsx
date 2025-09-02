c4dafff3158597ac20903181d1b634a2e2fca4f7
Md. Ridoy Hasan Kamrul <80632331+Md-Ridoy-Hasan-Kamrul@users.noreply.github.com>
2025-09-02 18:58:35 +0600

    (fix): fixed navbar

--- src/app/components/OpenAppointmentButton.jsx
+++ src/app/components/OpenAppointmentButton.jsx
@@ -1,6 +1,6 @@
-"use client";
-import { useState } from "react";
-import FirstModal from "./home/FirstModal";
+'use client';
+import { useState } from 'react';
+import FirstModal from './home/FirstModal';
 
 const OpenAppointmentButton = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -9,7 +9,7 @@ const OpenAppointmentButton = () => {
     <>
       <button
         onClick={() => setIsModalOpen(true)}
-        className="px-6 py-3 bg-purple-600 text-white rounded-lg hover:bg-purple-700 transition"
+        className='px-6 py-3 bg-[#A63EE7]  text-white rounded-lg hover:bg-[#A63EE9]  transition cursor-pointer'
       >
         Book a Free Discovery Call
       </button>
